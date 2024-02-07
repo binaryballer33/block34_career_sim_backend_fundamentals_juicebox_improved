9bda8bd5b7239e404c055e0d4b7818ffcf5baee0
Shaquille Mandy <shaqmandy@gmail.com>
2024-02-07 15:48:00 -0300

    created seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,208 +1,106 @@
-// const {
-// 	client,
-// 	prisma,
-// 	createUser,
-// 	updateUser,
-// 	getAllUsers,
-// 	getUserById,
-// 	createPost,
-// 	updatePost,
-// 	getAllPosts,
-// 	getAllTags,
-// 	getPostsByTagName,
-// } = require("./index");
+const {
+	prisma,
+	createUser,
+	updateUser,
+	getAllUsers,
+	getUserById,
+	createPost,
+	updatePost,
+	getAllPosts,
+} = require("./index");
+const { faker } = require("@faker-js/faker");
 
-// async function dropTables() {
-// 	try {
-// 		console.log("Starting to drop tables...");
+async function dropTables() {
+	try {
+		console.log("Starting to drop tables...");
 
-// 		// have to make sure to drop in correct order
-// 		// await client.query(`
-// 		//   DROP TABLE IF EXISTS post_tags;
-// 		//   DROP TABLE IF EXISTS tags;
-// 		//   DROP TABLE IF EXISTS posts;
-// 		//   DROP TABLE IF EXISTS users;
-// 		// `);
+		// have to make sure to drop in correct order
+		await prisma.post.deleteMany();
+		await prisma.user.deleteMany();
 
-// 		await prisma.postTag.deleteMany();
-// 		await prisma.tag.deleteMany();
-// 		await prisma.post.deleteMany();
-// 		await prisma.user.deleteMany();
+		// need to do this in order to stop the sequence from continuing to increment
+		// and causing a primary key violation when we try to insert new records into the students table
+		await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
+		await prisma.$executeRaw`ALTER SEQUENCE "Post_id_seq" RESTART WITH 1`;
 
-// 		console.log("Finished dropping tables!");
-// 	} catch (error) {
-// 		console.error("Error dropping tables!");
-// 		throw error;
-// 	}
-// }
+		console.log("Finished dropping tables!");
+	} catch (error) {
+		console.error("Error dropping tables!");
+		throw error;
+	}
+}
 
-// async function createTables() {
-// 	try {
-// 		console.log("Starting to build tables...");
+async function createInitialUsers() {
+	try {
+		console.log("Starting To Create Initial Users...");
 
-// 		// await client.query(`
-// 		//   CREATE TABLE users (
-// 		//     id SERIAL PRIMARY KEY,
-// 		//     username varchar(255) UNIQUE NOT NULL,
-// 		//     password varchar(255) NOT NULL,
-// 		//     name varchar(255) NOT NULL,
-// 		//     location varchar(255) NOT NULL,
-// 		//     active boolean DEFAULT true
-// 		//   );
+		const adminUser = await createUser({
+			name: "Admin",
+			username: "admin",
+			password: "password",
+		});
 
-// 		//   CREATE TABLE posts (
-// 		//     id SERIAL PRIMARY KEY,
-// 		//     "authorId" INTEGER REFERENCES users(id),
-// 		//     title varchar(255) NOT NULL,
-// 		//     content TEXT NOT NULL,
-// 		//     active BOOLEAN DEFAULT true
-// 		//   );
+		const users = await Promise.all(
+			[...Array(4)].map(() =>
+				createUser({
+					name: faker.person.firstName(),
+					username: faker.internet.userName(),
+					password: faker.internet.password(),
+				})
+			)
+		);
 
-// 		//   CREATE TABLE tags (
-// 		//     id SERIAL PRIMARY KEY,
-// 		//     name varchar(255) UNIQUE NOT NULL
-// 		//   );
+		users.push(adminUser);
 
-// 		//   CREATE TABLE post_tags (
-// 		//     "postId" INTEGER REFERENCES posts(id),
-// 		//     "tagId" INTEGER REFERENCES tags(id),
-// 		//     UNIQUE ("postId", "tagId")
-// 		//   );
-// 		// `);
+		console.log("Users: ", users);
 
-// 		console.log("Finished building tables!");
-// 	} catch (error) {
-// 		console.error("Error building tables!");
-// 		throw error;
-// 	}
-// }
+		console.log("Finished creating users!");
+	} catch (error) {
+		console.error("Error creating users!");
+		throw error;
+	}
+}
 
-// async function createInitialUsers() {
-// 	try {
-// 		console.log("Starting to create users...");
+async function createInitialPosts() {
+	try {
+		console.log("Starting to create posts...");
 
-// 		await createUser({
-// 			username: "albert",
-// 			password: "bertie99",
-// 			name: "Al Bert",
-// 			location: "Sidney, Australia",
-// 		});
-// 		await createUser({
-// 			username: "sandra",
-// 			password: "2sandy4me",
-// 			name: "Just Sandra",
-// 			location: "Ain't tellin'",
-// 		});
-// 		await createUser({
-// 			username: "glamgal",
-// 			password: "soglam",
-// 			name: "Joshua",
-// 			location: "Upper East Side",
-// 		});
+		const authorIds = (await getAllUsers()).map((user) => user.id);
 
-// 		console.log("Finished creating users!");
-// 	} catch (error) {
-// 		console.error("Error creating users!");
-// 		throw error;
-// 	}
-// }
+		const posts = await Promise.all(
+			[...Array(10)].map(() =>
+				createPost({
+					title: faker.lorem.word(),
+					content: faker.lorem.paragraph(),
+					authorId:
+						authorIds[Math.floor(Math.random() * authorIds.length)],
+				})
+			)
+		);
 
-// async function createInitialPosts() {
-// 	try {
-// 		const [albert, sandra, glamgal] = await getAllUsers();
+		console.log("Finished creating posts!");
+		console.log("Posts: ", posts);
+	} catch (error) {
+		console.log("Error creating posts!");
+		throw error;
+	}
+}
 
-// 		console.log("Starting to create posts...");
-// 		await createPost({
-// 			authorId: albert.id,
-// 			title: "First Post",
-// 			content:
-// 				"This is my first post. I hope I love writing blogs as much as I love writing them.",
-// 			tags: ["#happy", "#youcandoanything"],
-// 		});
+async function rebuildDB() {
+	try {
+		await dropTables();
+		await createInitialUsers();
+		await createInitialPosts();
+	} catch (error) {
+		console.log("Error during rebuildDB");
+		throw error;
+	}
+}
 
-// 		await createPost({
-// 			authorId: sandra.id,
-// 			title: "How does this work?",
-// 			content: "Seriously, does this even do anything?",
-// 			tags: ["#happy", "#worst-day-ever"],
-// 		});
-
-// 		await createPost({
-// 			authorId: glamgal.id,
-// 			title: "Living the Glam Life",
-// 			content: "Do you even? I swear that half of you are posing.",
-// 			tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
-// 		});
-// 		console.log("Finished creating posts!");
-// 	} catch (error) {
-// 		console.log("Error creating posts!");
-// 		throw error;
-// 	}
-// }
-
-// async function rebuildDB() {
-// 	try {
-// 		// client.connect();
-// 		await dropTables();
-// 		await createTables();
-// 		await createInitialUsers();
-// 		await createInitialPosts();
-// 	} catch (error) {
-// 		console.log("Error during rebuildDB");
-// 		throw error;
-// 	}
-// }
-
-// async function testDB() {
-// 	try {
-// 		console.log("Starting to test database...");
-
-// 		console.log("Calling getAllUsers");
-// 		const users = await getAllUsers();
-// 		console.log("Result:", users);
-
-// 		console.log("Calling updateUser on users[0]");
-// 		const updateUserResult = await updateUser(users[0].id, {
-// 			name: "Newname Sogood",
-// 			location: "Lesterville, KY",
-// 		});
-// 		console.log("Result:", updateUserResult);
-
-// 		console.log("Calling getAllPosts");
-// 		const posts = await getAllPosts();
-// 		console.log("Result:", posts);
-
-// 		console.log("Calling updatePost on posts[0]");
-// 		const updatePostResult = await updatePost(posts[0].id, {
-// 			title: "New Title",
-// 			content: "Updated Content",
-// 		});
-// 		console.log("Result:", updatePostResult);
-
-// 		console.log("Calling updatePost on posts[1], only updating tags");
-// 		const updatePostTagsResult = await updatePost(posts[1].id, {
-// 			tags: ["#youcandoanything", "#redfish", "#bluefish"],
-// 		});
-// 		console.log("Result:", updatePostTagsResult);
-
-// 		console.log("Calling getUserById with 1");
-// 		const albert = await getUserById(1);
-// 		console.log("Result:", albert);
-
-// 		console.log("Calling getAllTags");
-// 		const allTags = await getAllTags();
-// 		console.log("Result:", allTags);
-
-// 		console.log("Calling getPostsByTagName with #happy");
-// 		const postsWithHappy = await getPostsByTagName("#happy");
-// 		console.log("Result:", postsWithHappy);
-
-// 		console.log("Finished database tests!");
-// 	} catch (error) {
-// 		console.log("Error during testDB");
-// 		throw error;
-// 	}
-// }
-
-// rebuildDB().then(testDB).catch(console.error);
-// // .finally(() => client.end());
+try {
+	rebuildDB();
+} catch (error) {
+	console.log("Error during rebuildDB", error);
+} finally {
+	prisma.$disconnect();
+}
--- public/README.md
+++
@@ -1,5 +0,0 @@
-# Block 34 - Juicebox
-
-## Introduction
-
-In this assessment, you'll be provided this GitHub repo with details for a full operational CRUD API that uses Express.JS, PostgreSQL, and other technologies that you've been trained on. 
